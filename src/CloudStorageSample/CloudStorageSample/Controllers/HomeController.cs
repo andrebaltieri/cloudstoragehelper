6d8a3dbfc8f318dbd8b8a86429c93cf536aad38b
Andre Baltieri <andrebaltieri@hotmail.com>
2014-01-15 12:13:23 -0200

    Versão 1.0

---
+++ src/CloudStorageSample/CloudStorageSample/Controllers/HomeController.cs
@@ -0,0 +1,39 @@
+﻿using CloudStorageSample.Utils;
+using Microsoft.WindowsAzure.StorageClient;
+using System.Collections.Generic;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CloudStorageSample.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            CloudBlobContainer blobContainer = 
+                CloudStorageHelper.GetCloudBlobContainer("client-images");
+
+            List<string> blobs = new List<string>();
+            foreach (var blobItem in blobContainer.ListBlobs())
+                blobs.Add(blobItem.Uri.ToString());
+
+            return View(blobs);
+        }
+
+        [HttpPost]
+        [ActionName("Index")]
+        public ActionResult Index_Post(HttpPostedFileBase fileBase)
+        {
+            if (fileBase.ContentLength > 0)
+            {
+                CloudBlobContainer blobContainer = 
+                    CloudStorageHelper.GetCloudBlobContainer("client-images");
+                
+                CloudBlob blob = blobContainer.GetBlobReference(fileBase.FileName);
+                blob.UploadFromStream(fileBase.InputStream);
+            }
+
+            return RedirectToAction("Index");
+        }   
+    }
+}
---
+++ src/CloudStorageSample/CloudStorageSample/Utils/CloudStorageHelper.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.WindowsAzure;
+using Microsoft.WindowsAzure.ServiceRuntime;
+using Microsoft.WindowsAzure.StorageClient;
+
+namespace CloudStorageSample.Utils
+{
+    public static class CloudStorageHelper
+    {
+        public static CloudBlobContainer GetCloudBlobContainer(string container, bool createContainerIfNotExists = false)
+        {
+            CloudStorageAccount storageAccount =
+                CloudStorageAccount.Parse("DefaultEndpointsProtocol=https;AccountName=[ACCOUNTNAME];AccountKey=[ACCOUNTKEY]");
+
+            CloudBlobClient blobClient = storageAccount.CreateCloudBlobClient();
+            CloudBlobContainer blobContainer = blobClient.GetContainerReference(container);
+            
+            if (blobContainer.CreateIfNotExist())
+            {
+                if (createContainerIfNotExists)
+                    blobContainer.SetPermissions(new BlobContainerPermissions { PublicAccess = BlobContainerPublicAccessType.Blob });
+                else
+                    return null;
+            }
+
+            return blobContainer;
+        }
+    }
+}
+
